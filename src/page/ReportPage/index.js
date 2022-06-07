06bbc9bec797ea027979e8dc9182a8f1dc551199
gron1gh1 <gron1gh1@gmail.com>
2022-06-08 03:10:50 +0900

    add: reportPage chakra ui

--- src/page/ReportPage/index.js
+++ src/page/ReportPage/index.js
@@ -1,31 +1,42 @@
 import React, {useState} from "react";
 import {Link, useHistory, useNavigate, useParams} from "react-router-dom";
 import MDEditor from "@uiw/react-md-editor";
-import {Button} from "react-bootstrap";
+import {Box, Flex, Stack, Text,Button, useColorModeValue} from "@chakra-ui/react";
 
 function ReportPage() {
     const {id} = useParams();
     const [report, setReport] = useState();
-   const navigate = useNavigate();
-    return (
-        <div style={{width: "80vw", marginTop: "10px"}}>
-            <div>
-                <h1 style={{textAlign: "center", marginBottom: "10px"}}>MIPS Simulator 보고서</h1>
-            </div>
-            <div data-color-mode="light">
-                <MDEditor
-                    value={report}
-                    onChange={setReport}
-                    height={500}
-                    color="#fff"
-                />
-            </div>
-            <div style={{float: "right", marginTop: "10px"}}>
-                <Button style={{width: "100px"}}>저장</Button>
-                <Button onClick={() => navigate(-1)} style={{width: "100px",marginLeft:"5px"}} variant={"secondary"}>이전</Button>
-            </div>
-        </div>
-    );
+    const navigate = useNavigate();
+
+    return <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'} p={5}>
+        <Stack width={1000}>
+            <Box
+                rounded={'lg'}
+                bg={useColorModeValue('white', 'gray.700')}
+                boxShadow={'lg'}
+                p={8}
+
+            >
+                <Text textAlign={"center"} marginBottom={5}>MIPS Simulator 보고서</Text>
+                <Stack data-color-mode="light">
+                    <MDEditor
+                        value={report}
+                        onChange={setReport}
+                        height={500}
+                        color="#fff"
+                    />
+                </Stack>
+                <div style={{float: "right", marginTop: "10px"}}>
+                    <Button style={{width: "100px"}} colorScheme={"teal"}>저장</Button>
+                    <Button onClick={() => navigate(-1)} style={{width: "100px", marginLeft: "5px"}}>이전</Button>
+                </div>
+            </Box>
+        </Stack>
+    </Flex>
+
 }
 
 export default ReportPage;
